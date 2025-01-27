945bdd352e214683e43a02b5f5f313cc22104878
Mihai-Alexandru Soare <85075500+atomicul@users.noreply.github.com>
2025-01-27 18:37:44 +0200

    v1.1.4
    * Fix `BACKEND_URL` variable to allow a trailing '/'
    * Set up backend to work with clangd
    * Add missing build arg from the Dockerfile
    * Revert Crow version to 1.0-5 because of some issues with the browser's preflight request
    * Lint main.cpp file
    * Fix vcpkg install in Dockerfile

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-
+#include <filesystem>
 #include <crow.h>
 #include <crow/middlewares/cors.h>
 
@@ -12,29 +11,33 @@ int main() {
 
     auto *config = SingletonProvider::configuration();
 
-    CROW_ROUTE(app, "/<string>").methods(crow::HTTPMethod::Post)([config](const crow::request &req, const std::string &name) {
-        if (req.body.size() > config->max_size())
-            return crow::response(413);
+    CROW_ROUTE(app, "/<string>")
+        .methods(crow::HTTPMethod::Post)(
+            [config](const crow::request &req, const std::string &name) {
+              if (req.body.size() > config->max_size())
+                return crow::response(413);
 
-        const std::string save_location = config->location() + name;
+              const std::string save_location = config->location() + name;
 
-        if (!std::filesystem::exists(save_location)) {
-            std::ofstream(save_location) << req.body;
+              if (!std::filesystem::exists(save_location)) {
+                std::ofstream(save_location) << req.body;
 
-            return crow::response(204);
-        }
-
-        constexpr int max_loop = INT_MAX;
-        for (int i = 1; i<max_loop; i++) {
-            const std::string new_location = (std::stringstream() << save_location << " (" << i << ")").str();
-            if (!std::filesystem::exists(new_location)) {
-                std::ofstream(new_location) << req.body;
                 return crow::response(204);
-            }
-        }
+              }
 
-        return crow::response(500);
-    });
+              constexpr int max_loop = INT_MAX;
+              for (int i = 1; i < max_loop; i++) {
+                const std::string new_location =
+                    (std::stringstream() << save_location << " (" << i << ")")
+                        .str();
+                if (!std::filesystem::exists(new_location)) {
+                  std::ofstream(new_location) << req.body;
+                  return crow::response(204);
+                }
+              }
+
+              return crow::response(500);
+            });
 
     app
     .port(config->port())
